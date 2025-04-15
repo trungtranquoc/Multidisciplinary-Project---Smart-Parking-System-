8331e6f50fd736a741c0fc3a672fd0cfb929511c
lhem43 <ledominhanh@gmail.com>
2025-04-15 17:38:14 +0700

    Update user api

--- server/src/routers/user.py
+++ server/src/routers/user.py
@@ -12,13 +12,15 @@ async def get_parking_status():
     return status
 
 @router.get("/parking_history")
-async def get_parking_history(student_id: int = Depends(get_current_user)):
+async def get_parking_history(student_data: dict = Depends(get_current_user)):
+    student_id = student_data.get('student_id')
     logs = await db.parking_logs.find({"student_id": student_id}).to_list(length=None)
     return logs
 
 @router.get("/current_parking")
-async def get_current_parking(student_id: int = Depends(get_current_user)):
-    listparking = await db.parking_logs.find({"student_id": student_id, "exit_date": None}).to_list(length=None)
+async def get_current_parking(student_data: dict = Depends(get_current_user)):
+    student_id = student_data.get('student_id')
+    listparking = await db.parking_logs.find({"student_id": student_id, "exit": None}).to_list(length=None)
     status = await db.parking_status.find_one()
     if not status:
         status = {
@@ -38,7 +40,8 @@ async def get_current_parking(student_id: int = Depends(get_current_user)):
     }
 
 @router.get("/personal_information")
-async def get_personal_information(student_id: int = Depends(get_current_user)):
+async def get_personal_information(student_data: dict = Depends(get_current_user)):
+    student_id = student_data.get('student_id')
     account_info = await db.accounts.find_one({"student_id": student_id})
     if not account_info:
         raise HTTPException(status_code=404, detail="Account information not found.")
