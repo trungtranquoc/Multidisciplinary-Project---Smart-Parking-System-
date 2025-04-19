a4b7102d4c3cb080770dafae97ff2f9dd0c05796
Nouby_Le <lephonghao111004@gmail.com>
2025-04-19 19:50:51 +0700

    Admin Dashboard

--- src/components/WorkingTimeProgress.jsx
+++ src/components/WorkingTimeProgress.jsx
@@ -5,16 +5,12 @@ import {
 } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
-const SlotProgress = ({ total = 1000, available = 756}) => {
-  const percentage = Math.round((available / total) * 100);
-
-  const getTextColor = () => {
-    if (percentage < 10) return "text-red-600";
-    if (percentage >= 10 && percentage <= 50) return "text-orange-500";
-    if (percentage > 50) return "text-green-600";
-    return "text-blue-600";
-  };
-
+const WorkTimeProgess = ({ total = 17 * 60 * 60, work = 11 * 60 * 60 + 58 * 60 + 15 }) => {
+  const rHour = Math.floor((total - work) / 3600);
+  const rMin = Math.floor((total - rHour * 3600 - work) / 60)
+  const rSec = total - rHour * 3600 - work - 60 * rMin
+    console.log(total)
+    console.log(work)
   return (
     <div className="relative w-48 h-48">
       {/* Inner Blue Ring */}
@@ -33,13 +29,13 @@ const SlotProgress = ({ total = 1000, available = 756}) => {
       {/* Outer Progress Ring */}
       <div className="absolute inset-0">
         <CircularProgressbar
-          value={percentage}
+          value={work / total * 100}
           strokeWidth={8}
           styles={buildStyles({
             rotation: 0, // 12 o'clock start
             strokeLinecap: "round", // rounded ends = natural gap
             pathColor: "#22C55E", // green
-            trailColor: "#EF4444", // red
+            trailColor: "#FFFFFF", 
             textColor: "transparent",
           })}
         />
@@ -47,15 +43,15 @@ const SlotProgress = ({ total = 1000, available = 756}) => {
 
       {/* Center Text */}
       <div className="absolute inset-0 flex flex-col items-center justify-center">
-        <span className={`text-2xl font-bold ${getTextColor()}`}>
-          {percentage}%
+        <span className={`text-2xl font-bold text-blue-600`}>
+          {rHour}h
         </span>
         <span className="text-sm text-gray-600">
-          {available} / {total}
+          {rMin}m {rSec}s
         </span>
       </div>
     </div>
   );
 };
 
-export default SlotProgress;
+export default WorkTimeProgess;
